ea75b3e58c1909e538376abcca6cfaf7bbdd0317
moonrailgun <moonrailgun@gmail.com>
2017-08-03 11:49:47 +0800

    function, event and timer

--- test/test.js
+++ test/test.js
@@ -1,1 +1,9 @@
+const app = require('../../Core/')();
+const player = require('../../Player/');
 const chat = require('../');
+
+app.load(player);
+app.load(chat);
+app.run();
+app.reset();
+app.close();
