ee45bc4ed065f91be5ab22b1b94538317967723f
moonrailgun <moonrailgun@gmail.com>
2017-09-18 16:50:42 +0800

    修复一个调试信息的bug

--- test/test.js
+++ test/test.js
@@ -1,8 +1,10 @@
 const app = require('../../Core/')();
 const player = require('../../Player/');
+const actor = require('../../Actor/');
 const chat = require('../');
 
 app.load(player);
+app.load(actor);
 app.load(chat);
 app.run();
 app.reset();
